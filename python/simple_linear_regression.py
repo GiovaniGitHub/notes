00cc68da4013b40091423e6678c203e2cf08be6a
GiovaniGitHub <giovani.angelo@gmail.com>
2021-07-04 12:38:51 -0300

    Change function to increase the legibility.

--- python/simple_linear_regression.py
+++ python/simple_linear_regression.py
@@ -4,15 +4,12 @@ from math import sqrt
 def estimate_coef(x, y):
     n = len(x)
 
-    m_x = x.mean()
-    m_y = y.mean()
-
-    SS_xy = sum(y*x.T) - n*m_y*m_x
-    SS_xx = sum(x*x.T) - n*m_x*m_x
-    SS_yy = sum(y*y.T) - n*m_y*m_y
+    SS_xy = sum(y*x.T) - y.sum()*x.sum()/n
+    SS_xx = sum(x*x.T) - x.sum()*x.sum()/n
+    SS_yy = sum(y*y.T) - y.sum()*y.sum()/n
 
     b_1 = SS_xy / SS_xx
-    b_0 = m_y - b_1*m_x
+    b_0 = y.mean() - b_1*x.mean()
     
     r = SS_xy/(sqrt(SS_xx*SS_yy))
 
