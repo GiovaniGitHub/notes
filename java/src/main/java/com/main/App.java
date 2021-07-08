faeec83100e6f637c9a868573eab18a2fd852e8e
GiovaniGitHub <giovani.angelo@gmail.com>
2021-07-08 08:30:11 -0300

    Add plot in java version.

--- java/src/main/java/com/main/App.java
+++ java/src/main/java/com/main/App.java
@@ -1,7 +1,6 @@
 package com.main;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -9,10 +8,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 
-public final class App {
-    private App() {}
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-    public static void main(String[] args) throws IOException {
+public class App extends Application {
+
+    private Map<String, List<Double>> readDataset() {
         Map<String, List<Double>> dataset = new HashMap<String, List<Double>>();
         dataset.put("income", new ArrayList<>());
         dataset.put("happiness", new ArrayList<>());
@@ -23,13 +29,41 @@ public final class App {
                 dataset.get("income").add(Double.parseDouble(l.get(1)));
                 dataset.get("happiness").add(Double.parseDouble(l.get(2)));
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
+        return dataset;
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Map<String, List<Double>> dataset = readDataset();
         Map<String, Double> m = SimpleLinearRegression.estimateCoef(dataset.get("income"), dataset.get("happiness"));
         double[] yHat = new double[dataset.get("income").size()];
         for (int i = 0; i < dataset.get("income").size(); i++) {
             yHat[i] = dataset.get("income").get(i) * m.get("b1") + m.get("b0");
         }
 
+        // stage.setTitle("Simple Linear Regression");
+        final ScatterChart<Number, Number> sc = new ScatterChart<>(new NumberAxis(), new NumberAxis());
+        sc.setTitle("Simple Linear Regression");
+        XYChart.Series series1 = new XYChart.Series();
+        XYChart.Series series2 = new XYChart.Series();
+        series1.setName("Original Data");
+        series2.setName("Regression");
+        for (int i = 0; i < dataset.get("income").size(); i++) {
+            series1.getData().add(new XYChart.Data(dataset.get("income").get(i), dataset.get("happiness").get(i)));
+            series2.getData().add(new XYChart.Data(dataset.get("income").get(i), yHat[i]));
+        }
+
+        sc.getData().addAll(series1, series2);
+        Scene scene = new Scene(sc);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
