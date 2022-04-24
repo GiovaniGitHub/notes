7b669c96e8a7737a8b04144f91b6975808f53197
GiovaniGitHub <giovani.angelo@gmail.com>
2022-04-24 11:11:10 -0300

    : .

--- julia/distances.jl
+++ julia/distances.jl
@@ -1,5 +1,5 @@
-using LinearAlgebra: norm
-using Distances:mahalanobis
+using LinearAlgebra: norm, dot
+using Statistics: mean, cov
 
 function euclidean(x, y)
     return sqrt(sum(x - y) .^2)
@@ -10,11 +10,11 @@ function sqeuclidean(x, y)
 end
 
 function cityblock(x, y)
-    return sum(abs(x - y))
+    return sum(abs.(x - y))
 end
 
 function total_variation(x, y)
-    return sum(abs(x - y)) / 2
+    return sum(abs.(x - y)) / 2
 end
 
 function jaccard(x, y)
@@ -26,30 +26,20 @@ function cosine_distance(x, y)
 end
 
 function correlation_distance(x, y)
-    return cosine_distance(x - mean(x), y - mean(y))
+    return cosine_distance(x .- mean(x), y .- mean(y))
 end
 
 function chi_square_distance(x, y)
     return sum((x - y).^2 / (x + y))
 end
 
-function kullback_leibler_divergence(x, y)
-    return sum(x .* log(x ./ y))
-end
-
-function generalized_kullback_leibler_divergence(x, y)
-    return sum(x .* log(x ./ y) - x + y)
-end
-
-function dice_distance(x, y)
-    return 2*(sum(min(x, y)))/(norm(x) + norm(y))
-end
-
-function jensen_shannon_divergence(x, y)
-    m = (x + y) / 2
-    return kullback_leibler_divergence(x, m) / 2 + kullback_leibler_divergence(m, y) / 2
-end
-
-function mahalanobis(x, y)
-    return sqrt(sum((x - y).^2)*cov(x, y))
-end
+all_functions = [
+        euclidean,
+        sqeuclidean,
+        cityblock,
+        total_variation,
+        jaccard,
+        cosine_distance,
+        correlation_distance,
+        chi_square_distance,
+        ];
--- julia/knn.jl
+++ julia/knn.jl
@@ -1,21 +1,21 @@
 using DataFrames
 using CSV
-using Distances
+# using Distances
 using StatsBase:mode
 using Formatting
 
 include("utils.jl")
-
+include("distances.jl")
 mutable struct KNN
     data::Array{Float64,2}
     target::Array{Float64,2}
 end
 
-function predict(x::Array{Float64,1}, n_neighborhood:: Int64, self::KNN)
+function predict(x::Array{Float64,1}, n_neighborhood:: Int64, self::KNN, distance::Function)
     heat_labels = zeros((n_neighborhood,2))
     heat_labels[:,1] .= Inf
     for i in 1:size(self.data,1)
-        d = euclidean(x, self.data[i,:])
+        d = distance(x, self.data[i,:])
         idx = findfirst(>(d), heat_labels[:,1])
         if idx !== nothing
             heat_labels[idx, 1] = d
@@ -40,12 +40,15 @@ function main()
     knn = KNN(x_train, y_train)
 
     n_neighborhood = 10
-    acc = sum([
-                y_test[i,1] === predict(x_test[i,:], n_neighborhood, knn)[1,1]
-                for i in 1:size(x_test,1)
-            ]) / size(y_test,1)
-    
-    printfmt("The accuracy is {:f}", acc)
+    for distance in all_functions
+        println(distance)
+        acc = sum([
+                    y_test[i,1] === predict(x_test[i,:], n_neighborhood, knn, distance)[1,1]
+                    for i in 1:size(x_test,1)
+                ]) / size(y_test,1)
+        printfmtln("With function {:s} the accuracy is {:f}", string(distance), acc)
+        
+    end
 end
 
 main()
