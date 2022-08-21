c9ee0c94f0fff0c74e27cce9218ef9fbe19501fc
GiovaniGitHub <giovani.angelo@gmail.com>
2022-08-21 18:30:46 -0300

    Add scatter plot method

--- rust/src/utils/io.rs
+++ rust/src/utils/io.rs
@@ -1,10 +1,11 @@
-use nalgebra::{Scalar};
-use std::{io::BufRead, str::FromStr};
-use plotly::common::Mode;
+use nalgebra::Scalar;
+use plotly::common::{Mode, Marker};
 use plotly::{Plot, Scatter};
+use std::{io::BufRead, str::FromStr};
+
 
 #[allow(non_camel_case_types)]
-pub fn parse_csv<f32, R>(input: R)  -> Result<(usize, usize, Vec<f32>), Box<dyn std::error::Error>>
+pub fn parse_csv<f32, R>(input: R) -> Result<(usize, usize, Vec<f32>), Box<dyn std::error::Error>>
 where
     f32: FromStr + Scalar,
     f32::Err: std::error::Error,
@@ -38,5 +39,41 @@ pub fn line_and_scatter_plot(x: Vec<f32>, y: Vec<Vec<f32>>, names: Vec<&str>) {
         );
         idx = idx + 1;
     }
+    plot.show();
+}
+
+pub fn scatter_plot(x: Vec<f32>, y: Vec<f32>, cls: Vec<usize>) {
+    let mut plot = Plot::new();
+    let mut unique_values = cls.clone();
+    unique_values.sort();
+    unique_values.dedup();
+
+    for value_label in unique_values {
+        let label_str = format!("Cluster {}", value_label);
+        let idxs: Vec<_> = cls
+            .iter()
+            .enumerate()
+            .filter(|&(_1, _0)| _0.eq(&value_label))
+            .map(|(index, _)| index)
+            .collect();
+        plot.add_trace(
+            Scatter::new(
+                x.iter()
+                    .enumerate()
+                    .filter(|(i, _)| idxs.contains(i))
+                    .map(|(_, v)| v.clone())
+                    .collect::<Vec<f32>>(),
+                y.iter()
+                    .enumerate()
+                    .filter(|(i, _)| idxs.contains(i))
+                    .map(|(_, v)| v.clone())
+                    .collect::<Vec<f32>>(),
+            )
+            .mode(Mode::Markers)
+            .name(&label_str)
+            .marker(Marker::new().size(12))
+        );
+    }
+
     plot.show();
 }
